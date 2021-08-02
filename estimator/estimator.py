d9e98fa6e62b1b2e84fc8a8bc546271cb2ff3b06
Yutong Xie <yutongxie98@gmail.com>
2021-08-03 07:22:42 +0800

    Update estimator.py

--- estimator/estimator.py
+++ estimator/estimator.py
@@ -36,38 +36,6 @@ class Estimator():
 
         dicts = [{} for _ in mols]
         for obj in self.objectives:
-            if obj == 'adv': continue
-            if obj == 'nov':
-                for i, fp in enumerate(fps_mols):
-                    sims = DataStructs.BulkTanimotoSimilarity(fp, self.fps_ref)
-                    dicts[i][obj] = 1. - max(sims)
-                continue
-            if obj == 'div':
-                for i, fp in enumerate(fps_mols):
-                    sims = DataStructs.BulkTanimotoSimilarity(fp, fps_mols)
-                    dicts[i][obj] = 1. - 1. * sum(sims) / len(fps_mols)
-                continue
             scores = get_scores(obj, mols)
             for i, mol in enumerate(mols):
                 dicts[i][obj] = scores[i]
-
-        if 'adv' in self.objectives:
-            graphs = [mol_to_dgl(mol) for mol in mols]
-            dataset = GraphDataset(graphs)
-            loader = DataLoader(dataset,
-                batch_size=self.batch_size,
-                collate_fn=GraphDataset.collate_fn
-            )
-
-            preds = []
-            for batch in loader:
-                with torch.no_grad():
-                    pred = self.discriminator(batch) # (batch_size, 2)
-                    pred = F.softmax(pred, dim=1)    # (batch_size, 2)
-                preds.append(pred[:, 1]) # (batch_size,)
-            preds = torch.cat(preds, dim=0).tolist() # (num_mols,)
-            for i, pred in enumerate(preds):
-                dicts[i]['adv'] = pred
-        return dicts
-
-
